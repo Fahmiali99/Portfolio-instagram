cc4ca453718d86eb8613fdeaa7ba9d21a82e44a3
Fahmi <fahmiali130499@gmail.com>
2022-09-19 16:02:12 +0700

    doneee

--- src/components/Body/About/index.js
+++ src/components/Body/About/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function index() {
   return (
     <div>
-      <p className="text-center">
+      <p className="text-center text-stone-900">
         Experienced in web developer, especially in frontend developer. I hope
         found new innovations for future.
       </p>

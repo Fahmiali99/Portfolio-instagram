7a4495a5f9c390c38022b391c82ba4d556f1bb3c
Fahmi <fahmiali130499@gmail.com>
2022-12-03 04:07:11 +0700

    change bg color theme

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -5,7 +5,10 @@ import Profile from "../Profile/Profile";
 
 function index() {
   return (
-    <div className=" flex justify-center h-auto w-full">
+    <div
+      className=" flex justify-center h-auto w-full "
+      style={{ background: "#2a303c" }}
+    >
       <div className=" md:w-2/5 w-screen  ">
         <Up />
         <div className="p-10">
